94a6baaff1f6c51738d3ac15ca81fd7b4cb364fa
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-07-08 22:40:33 +0200

    added gif for page

--- src/Fruits.js
+++ src/Fruits.js
@@ -1,11 +1,18 @@
-import React, { Component } from 'react'
-import Link from "react-router-dom"
-import "./Fruits.css"
+import React, { Component } from 'react';
+import Link from 'react-router-dom';
+import './Fruits.css';
 
-class Fruits extends Component{
-    render() {
-        return()
-    }
+class Fruits extends Component {
+	render() {
+		return (
+			<div className='Fruits'>
+				<img
+					src='https://media1.tenor.com/images/19ed71233d1b9db8a8ba35da94ad8f2c/tenor.gif?itemid=4808470'
+					alt='girl dancing with fruits'
+				/>
+			</div>
+		);
+	}
 }
 
 export default Fruits;
