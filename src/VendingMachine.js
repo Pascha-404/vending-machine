7ffd32aea1b0da58c5d63d068a023c2d5ffe0145
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-07-08 22:42:29 +0200

    finished structure and links

--- src/VendingMachine.js
+++ src/VendingMachine.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
-import {Route, Link, Switch, NavLink} from "react-router-dom"
+import {Link} from "react-router-dom"
 import "./VendingMachine.css"
 
 class VendingMachine extends Component{
     render() {
         return (
             <div className="VendingMachine">
-
+                <div className="VendingMachine-data">
+                    <p>Hello, I'm a Vendigmachine. What can I serve you?</p>
+                    <div className="VendingMachine-products">
+                        <Link to="/redbull">Redbull</Link>
+                        <Link to="/cheetos">Cheetos</Link>
+                        <Link to="/fruits">Fruits</Link>
+                    </div>
+                </div>
             </div>
         )
     }
