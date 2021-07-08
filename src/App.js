3d290489bd440082ee4420f8d160c7845d9ee645
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-07-08 22:39:59 +0200

    added navbar and routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Router, NavLink, Switch } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 import './App.css';
 import VendingMachine from './VendingMachine';
 import Redbull from './Redbull';
@@ -7,10 +7,30 @@ import Cheetos from './Cheetos';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
-  );
+		<div className='App'>
+			<nav className='App-nav'>
+				<NavLink activeClassName="nav-active" exact to='/'>Vendingmachine</NavLink>
+				<NavLink activeClassName="nav-active" exact to='/redbull'>Redbull</NavLink>
+				<NavLink activeClassName="nav-active" exact to='/fruits'>Fruits</NavLink>
+				<NavLink activeClassName="nav-active" exact to='/cheetos'>Cheetos</NavLink>
+			</nav>
+
+			<Switch>
+				<Route exact path='/'>
+					<VendingMachine />
+				</Route>
+				<Route exact path='/redbull'>
+					<Redbull />
+				</Route>
+				<Route exact path='/fruits'>
+					<Fruits />
+				</Route>
+				<Route exact path='/cheetos'>
+					<Cheetos />
+				</Route>
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
