8393c42484976448c5b88f4a6a0bec87dad44a54
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-07-12 12:30:08 +0200

    replaced image

--- src/Redbull.js
+++ src/Redbull.js
@@ -7,7 +7,7 @@ class Redbull extends Component {
 		return (
 			<div className='Redbull'>
 				<img
-					src='https://png2.cleanpng.com/sh/a326edefe7dd9444c149823adbfe0d91/L0KzQYm3UsAzN6l3iZH0aYP2gLBuTfVvbaNskZ9tcnnxe375hfQuaqZxhJ98b3b3PbX5if5sNZ50huV9ZYKwdbBskvd6NZD6ReRuZD3lhb3zTgBvb15uhdNwZT24cYi8UsgzO5Y9T9NvMj68SYK5UsQ0QWI6SakBMke3QoiCVcI0NqFzf3==/kisspng-energy-drink-red-bull-soft-drink-monster-energy-ou-red-bull-png-image-5a752823e87af2.9912243915176274279523.png'
+					src='http://assets.stickpng.com/images/580b57fcd9996e24bc43c1e9.png'
 					alt='redbull'
 				/>
 				<div className='Redbull-data'>
@@ -15,7 +15,7 @@ class Redbull extends Component {
 					<Link to='/'>GO BACK</Link>
 				</div>
 				<img
-					src='https://png2.cleanpng.com/sh/a326edefe7dd9444c149823adbfe0d91/L0KzQYm3UsAzN6l3iZH0aYP2gLBuTfVvbaNskZ9tcnnxe375hfQuaqZxhJ98b3b3PbX5if5sNZ50huV9ZYKwdbBskvd6NZD6ReRuZD3lhb3zTgBvb15uhdNwZT24cYi8UsgzO5Y9T9NvMj68SYK5UsQ0QWI6SakBMke3QoiCVcI0NqFzf3==/kisspng-energy-drink-red-bull-soft-drink-monster-energy-ou-red-bull-png-image-5a752823e87af2.9912243915176274279523.png'
+					src='http://assets.stickpng.com/images/580b57fcd9996e24bc43c1e9.png'
 					alt='redbull'
 				/>
 			</div>
